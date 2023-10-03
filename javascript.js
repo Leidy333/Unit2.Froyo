7b5aec8cd58304d85905ff1b23acca5e3c0b3786
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2023-10-02 21:23:41 -0500

    finishedAssignment

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// get user input for froyo flavors
+
+const userInputString = prompt(
+  "Please enter a list of comma-separated froyo flavors. No spaces!"
+);
+
+// turn user input into froyo array
+const froyoFlavors = userInputString.split(",");
+
+// creates froyo object
+const froyoCount = {};
+
+for (let i = 0; i < froyoFlavors.length; i++) {
+  // added variable to reduce writing code. Added trim to remove whitespaces in case of User Error
+  flavor = froyoFlavors[i].trim();
+  // if flavor is not a key froyoCount then value is 1 and remains that way until changed
+  if (!froyoCount[flavor]) {
+    froyoCount[flavor] = 1;
+  } else {
+    // if flavor is in froyoCount, then add 1 to the amount in the the value of for flavor
+    froyoCount[flavor]++;
+  }
+}
+
+console.table(froyoCount);
