5480ccac2e5bbe886b84ff9a99d0c3e796ca9ab2
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2023-10-02 21:43:04 -0500

    Added function findFroyoCount

--- javascript.js
+++ javascript.js
@@ -10,16 +10,20 @@ const froyoFlavors = userInputString.split(",");
 // creates froyo object
 const froyoCount = {};
 
-for (let i = 0; i < froyoFlavors.length; i++) {
-  // added variable to reduce writing code. Added trim to remove whitespaces in case of User Error
-  flavor = froyoFlavors[i].trim();
-  // if flavor is not a key froyoCount then value is 1 and remains that way until changed
-  if (!froyoCount[flavor]) {
-    froyoCount[flavor] = 1;
-  } else {
-    // if flavor is in froyoCount, then add 1 to the amount in the the value of for flavor
-    froyoCount[flavor]++;
+function findFroyoCount() {
+  for (let i = 0; i < froyoFlavors.length; i++) {
+    // added variable to reduce writing code. Added trim to remove whitespaces in case of User Error
+    flavor = froyoFlavors[i].trim();
+    // if flavor is not a key froyoCount then value is 1 and remains that way until changed
+    if (!froyoCount[flavor]) {
+      froyoCount[flavor] = 1;
+    } else {
+      // if flavor is in froyoCount, then add 1 to the amount in the the value of for flavor
+      froyoCount[flavor]++;
+    }
   }
+  return froyoCount;
 }
 
+findFroyoCount();
 console.table(froyoCount);
