e2fcbf4cb0ec7411719dc5b34a606896d5bf0ef3
Leidy333 <tamy_2011@yahoo.com>
2023-10-05 13:50:08 -0700

    able to produce table with falvors

--- froyo.js
+++ froyo.js
@@ -4,3 +4,9 @@ const order = prompt("Please add your order with commas between each flavor. Tha
 // Turn order into array named flavorOrdered
 const flavorOrdered = order.split(",");
 
+// create object from array
+const flavors = {};
+// use forEach loop to fill object
+    flavorOrdered.forEach((val) => (flavors[val] = (flavors[val] || 0)+1));
+
+console.table(flavors)
