b55266acb68bc581572354cce92110f9472ef9e8
Leidy333 <tamy_2011@yahoo.com>
2023-10-05 11:11:51 -0700

    created new branch and code

---
+++ froyo.js
@@ -0,0 +1,6 @@
+//start by making prompt for order
+const order = prompt("Please add your order with commas between each flavor. Thank YOU!!");
+
+// Turn order into array named flavorOrdered
+const flavorOrdered = order.split(",");
+
